b158a98e1df525095293fa1c97ee3ebe9c21c656
Anusha G <Anusha G@TYSS-Anusha-G>
2023-12-19 11:54:25 +0530

    first

---
+++ mavenjenkinsproject/src/test/java/login/login.java
@@ -0,0 +1,13 @@
+package login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class login {
+	@Test
+	public void login()
+	{
+		Reporter.log("login successfully");
+	}
+
+}
