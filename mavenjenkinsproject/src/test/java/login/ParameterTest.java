10fea1d03ac4295c3dc60335e97f513e743c80e8
Anusha G <Anusha G@TYSS-Anusha-G>
2023-12-19 13:21:59 +0530

    push changes

--- mavenjenkinsproject/src/test/java/login/ParameterTest.java
+++ mavenjenkinsproject/src/test/java/login/ParameterTest.java
@@ -1,12 +1,13 @@
 package login;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ParameterTest {
 	@Test
 	public void test() {
-		ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		String BROWSER = System.getProperty("Url");
 		driver.get(BROWSER);
 		
